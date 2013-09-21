79027b01fc5b0a021035da19d628b0a862cae131
tae hae kim <iMac@tae-hae-kims-imac.local>
2013-09-20 23:03:46 -0700

    First commit and README update

---
+++ README.md
@@ -0,0 +1,5 @@
+# Bloccit
+
+A Reddit-style application to demonstrate foundation programming and web development skills.
+
+Made with help from [Bloc](http://bloc.io)
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bloccit::Application.config.secret_key_base = '06a1e277a8d121807114e3038b980ec087af48e31d33f48614dec81463d7113e8fc52b5c513bc57b2256b9ab7fc7385c23c8577c99057f3bb4f8e302481180d1'
