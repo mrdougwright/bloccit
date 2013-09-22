53d6dc12d1585fee14e67867a9d0b9e2672d2867
tae hae kim <iMac@tae-hae-kims-imac.local>
2013-09-21 19:54:15 -0700

    Create Post resource and update index and show views

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Bloccit::Application.routes.draw do
-  get "welcome/index"
-  get "welcome/about"
+  resources :posts
+
+  match "about" => 'welcome#about', via: :get
+  
   root to: "welcome#index"
 end
