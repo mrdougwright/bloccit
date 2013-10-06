8c887e515d60f86e95e822c19d1762c0cb0a9f54
Doug Wright <mrdougwright@gmail.com>
2013-10-06 12:52:59 -0700

    fix prod config for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,8 +78,5 @@ Bloccit::Application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
-  # devise added:
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.perform_deliveries = true
+  
 end
